64005d4d8cf644711cb62c90b0662d082139f592
Leo Wu <leo.wu.kw@gmail.com>
2025-02-28 11:53:39 +0800

    support android

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {mapping} from "./mapping";
 import * as moment from 'moment-timezone';
 import 'moment/locale/zh-hk';
 import {importLog} from "./saveLog";
+import gplay from "google-play-scraper";
 
 moment.tz.setDefault("Asia/Hong_Kong");
 
@@ -65,192 +66,207 @@ function searchBracket(text: string): number {
   moment.tz.setDefault("Asia/Hong_Kong");
   const jar = rp.jar()
 
+  let androidVersionRes = await gplay.app({appId: 'com.lihkg.app'})
+
+  console.log(androidVersionRes.version)
+  let androidVersion = androidVersionRes.version
+
   let versionRes = await rp("https://itunes.apple.com/lookup?bundleId=com.lihkg.forum-ios")
   let version: string = JSON.parse(versionRes)["results"][0]["version"]
 
   console.log("iOS version : " + version)
 
-  let res = await rp({
-    method: "POST",
-    url: "https://lihkg.com/api_v2/system/property?asset_v=0",
-    headers: {
-      "X-LI-USER": "1",
-      "Content-Type": "application/x-www-form-urlencoded",
-      "User-Agent": `LIHKG/4.00 iOS/18.3.1 iPhone/iPhone16,1`,
-    },
-    jar: jar,
-  })
+  let verMap = {
+    "ios":`LIHKG/4.00 iOS/18.3.1 iPhone/iPhone16,1`,
+    "android":`LIHKG/${androidVersion} Android/Pixel1`
+  }
 
-  let assetUrl = JSON.parse(res)["response"]["asset"]["patch"][0]["url"]
-  let fileName: string = assetUrl.split("/")[assetUrl.split("/").length - 1]
-  console.log("assetUrl : " + assetUrl)
+  for (let platform in verMap) {
 
-  fs.writeFileSync(`property/${version}-${fileName.replace(".zip", "")}.json`, JSON.stringify(JSON.parse(res), null, 2))
-  rp({
-    method: "get",
-    url: assetUrl,
-    jar: jar,
-    encoding: null,
-    headers: {
-      "User-Agent": `LIHKG/${version} iOS/14.7.1 iPhone/iPhone 6s`
-    },
-  }).then(async res => {
-    fs.writeFileSync(fileName, res)
+    console.log(verMap[platform]);
 
-    const fileSizeInBytes = fs.statSync(fileName).size;
-    const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+    let res = await rp({
+      method: "POST",
+      url: "https://lihkg.com/api_v2/system/property?asset_v=0",
+      headers: {
+        "X-LI-USER": "1",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "User-Agent": verMap[platform],
+      },
+      jar: jar,
+    })
 
-    if (fileSizeInMegabytes > 0) {
-      let folderName = fileName.replace(".zip", "").replace(".", "/")
+    let assetUrl = JSON.parse(res)["response"]["asset"]["patch"][0]["url"]
+    let fileName: string = assetUrl.split("/")[assetUrl.split("/").length - 1]
+    console.log("assetUrl : " + assetUrl)
 
-      const zip = new AdmZip(fileName);
+    fs.writeFileSync(`property/${version}-${fileName.replace(".zip", "")}.json`, JSON.stringify(JSON.parse(res), null, 2))
+    rp({
+      method: "get",
+      url: assetUrl,
+      jar: jar,
+      encoding: null,
+      headers: {
+        "User-Agent": `LIHKG/${version} iOS/14.7.1 iPhone/iPhone 6s`
+      },
+    }).then(async res => {
+      fs.writeFileSync(fileName, res)
 
-      zip.extractAllTo(folderName, true);
+      const fileSizeInBytes = fs.statSync(fileName).size;
+      const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
 
-      fs.rmdirSync("assets/faces", {recursive: true})
-      fs.rmdirSync("assets/faces_png", {recursive: true})
+      if (fileSizeInMegabytes > 0) {
+        let folderName = fileName.replace(".zip", "").replace(".", "/")
 
+        const zip = new AdmZip(fileName);
 
-      fs.moveSync(`${folderName}/limoji.json`, "assets/limoji.json", {overwrite: true})
-      fs.moveSync(`${folderName}/assets/faces`, "assets/faces", {overwrite: true})
-      fs.moveSync(`${folderName}/assets/faces_png`, "assets/faces_png", {overwrite: true})
+        zip.extractAllTo(folderName, true);
 
-
-      const {document}: DOMWindow = await getWebWindow("https://lihkg.com", jar, version)
-
-      let bodyScripts = document.getElementsByTagName("body")[0]
-        .getElementsByTagName("script")
-
-      let mainScriptUrl = ""
-
-      for (let i = 0; i < bodyScripts.length; i++) {
-        let bodyScript = bodyScripts[i]
-        let bodyScriptSrc = bodyScript.getAttribute("src")
-        if (
-          bodyScriptSrc != undefined &&
-          bodyScriptSrc.indexOf("lihkg.com") >= 0 &&
-          bodyScriptSrc.endsWith("main.js")
-        ) {
-          mainScriptUrl = bodyScriptSrc
-        }
-      }
-
-      let mainJsResStr: string = await rp({
-        method: "GET",
-        url: mainScriptUrl,
-        jar: jar,
-        headers: {
-          "User-Agent": `LIHKG/${version} iOS/14.7.1 iPhone/iPhone 6s`
-        },
-      })
+        fs.rmdirSync("assets/faces", {recursive: true})
+        fs.rmdirSync("assets/faces_png", {recursive: true})
 
 
-      let startPos = mainJsResStr.indexOf("{normal:{icons:{\"assets/faces/normal/")
-      mainJsResStr = mainJsResStr.substring(startPos)
+        fs.moveSync(`${folderName}/limoji.json`, "assets/limoji.json", {overwrite: true})
+        fs.moveSync(`${folderName}/assets/faces`, "assets/faces", {overwrite: true})
+        fs.moveSync(`${folderName}/assets/faces_png`, "assets/faces_png", {overwrite: true})
 
-      let endPos = searchBracket(mainJsResStr)
-      mainJsResStr = mainJsResStr.substring(0, endPos + 1)
 
-      mainJsResStr = mainJsResStr.replace(/!0/g, "1")
+        const {document}: DOMWindow = await getWebWindow("https://lihkg.com", jar, version)
 
-      if (fs.existsSync("./src/tmp.ts")) {
-        fs.removeSync("./src/tmp.ts")
-      }
+        let bodyScripts = document.getElementsByTagName("body")[0]
+            .getElementsByTagName("script")
 
-      fs.writeFileSync("./src/mainJson.ts", `export default ${mainJsResStr}`, "utf8");
+        let mainScriptUrl = ""
 
-      // fix the key not match for "xm" and "Xmas"
-      fs.writeFileSync("./assets/main.json", JSON.stringify(require("./mainJson").default, null, 2).replace("\"xmas\"", "\"xm\""))
-
-      fs.removeSync("./src/mainJson.ts")
-
-      const limoji = require('../assets/limoji.json');
-      const mainJson = require('../assets/main.json')
-
-      let mainContent = "| Code | Name | Preview | View |\n"
-      mainContent += "| --- | --- | --- | --- |\n"
-      mainContent += "| (All) | N/A | N/A | [View](./view/all.md) |\n"
-
-      let allContent = "# All icons\n"
-
-      for (const packDict of limoji.emojis) {
-        let cat = packDict.cat
-
-        let icons = packDict["icons"]
-
-        let specialList = []
-
-        if (mainJson[cat] && Object.keys(mainJson[cat]).indexOf("special") >= 0) {
-          let specialDict: { [key: string]: string } = mainJson[cat]["special"]
-
-          if (Object.keys(specialDict).length > 0) {
-            Object.keys(specialDict).forEach(gifPath => {
-              let code = specialDict[gifPath]
-              let pngPath = gifPath.replace(/\.gif/g, ".png").replace(/faces/g, "faces_png")
-              specialList.push([code, gifPath, pngPath])
-            })
+        for (let i = 0; i < bodyScripts.length; i++) {
+          let bodyScript = bodyScripts[i]
+          let bodyScriptSrc = bodyScript.getAttribute("src")
+          if (
+              bodyScriptSrc != undefined &&
+              bodyScriptSrc.indexOf("lihkg.com") >= 0 &&
+              bodyScriptSrc.endsWith("main.js")
+          ) {
+            mainScriptUrl = bodyScriptSrc
           }
         }
 
-        let merge = []
-        merge.push(...icons)
-        merge.push(...specialList)
+        let mainJsResStr: string = await rp({
+          method: "GET",
+          url: mainScriptUrl,
+          jar: jar,
+          headers: {
+            "User-Agent": `LIHKG/${version} iOS/14.7.1 iPhone/iPhone 6s`
+          },
+        })
 
-        let viewContent = ""
-        viewContent += `## ${cat} [${mapping[cat] ? mapping[cat] : cat}]\n`
 
-        let showOn = packDict["show_on_2"]
+        let startPos = mainJsResStr.indexOf("{normal:{icons:{\"assets/faces/normal/")
+        mainJsResStr = mainJsResStr.substring(startPos)
 
-        if (showOn) {
-          let showOnArray: any[] = showOn
-          viewContent += `\n`
-          viewContent += `### Show On\n`
-          for (const showOnObj of showOnArray) {
-            if (Object.keys(showOnObj).includes("keywords")) {
-              viewContent += `Keywords : ${(showOnObj["keywords"] as string[]).join(", ")}\n`
-            } else if (Object.keys(showOnObj).includes("start_time") && Object.keys(showOnObj).includes("end_time")) {
-              viewContent += `From ${moment(showOnObj["start_time"]).tz("Asia/Hong_Kong").format()}\n\n`
-              viewContent += `To ${moment(showOnObj["end_time"]).tz("Asia/Hong_Kong").format()}\n`
-            } else if (Object.keys(showOnObj).includes("user_ids")) {
-              viewContent += `User ID: ${(showOnObj["user_ids"] as string[]).join(", ")}\n`
-            } else {
-              console.log("cat" + cat)
-              console.log("showOnObj")
-              console.log(showOnObj)
+        let endPos = searchBracket(mainJsResStr)
+        mainJsResStr = mainJsResStr.substring(0, endPos + 1)
+
+        mainJsResStr = mainJsResStr.replace(/!0/g, "1")
+
+        if (fs.existsSync("./src/tmp.ts")) {
+          fs.removeSync("./src/tmp.ts")
+        }
+
+        fs.writeFileSync("./src/mainJson.ts", `export default ${mainJsResStr}`, "utf8");
+
+        // fix the key not match for "xm" and "Xmas"
+        fs.writeFileSync("./assets/main.json", JSON.stringify(require("./mainJson").default, null, 2).replace("\"xmas\"", "\"xm\""))
+
+        fs.removeSync("./src/mainJson.ts")
+
+        const limoji = require('../assets/limoji.json');
+        const mainJson = require('../assets/main.json')
+
+        let mainContent = "| Code | Name | Preview | View |\n"
+        mainContent += "| --- | --- | --- | --- |\n"
+        mainContent += "| (All) | N/A | N/A | [View](./view/all.md) |\n"
+
+        let allContent = "# All icons\n"
+
+        for (const packDict of limoji.emojis) {
+          let cat = packDict.cat
+
+          let icons = packDict["icons"]
+
+          let specialList = []
+
+          if (mainJson[cat] && Object.keys(mainJson[cat]).indexOf("special") >= 0) {
+            let specialDict: { [key: string]: string } = mainJson[cat]["special"]
+
+            if (Object.keys(specialDict).length > 0) {
+              Object.keys(specialDict).forEach(gifPath => {
+                let code = specialDict[gifPath]
+                let pngPath = gifPath.replace(/\.gif/g, ".png").replace(/faces/g, "faces_png")
+                specialList.push([code, gifPath, pngPath])
+              })
             }
           }
-          viewContent += `\n`
+
+          let merge = []
+          merge.push(...icons)
+          merge.push(...specialList)
+
+          let viewContent = ""
+          viewContent += `## ${cat} [${mapping[cat] ? mapping[cat] : cat}]\n`
+
+          let showOn = packDict["show_on_2"]
+
+          if (showOn) {
+            let showOnArray: any[] = showOn
+            viewContent += `\n`
+            viewContent += `### Show On\n`
+            for (const showOnObj of showOnArray) {
+              if (Object.keys(showOnObj).includes("keywords")) {
+                viewContent += `Keywords : ${(showOnObj["keywords"] as string[]).join(", ")}\n`
+              } else if (Object.keys(showOnObj).includes("start_time") && Object.keys(showOnObj).includes("end_time")) {
+                viewContent += `From ${moment(showOnObj["start_time"]).tz("Asia/Hong_Kong").format()}\n\n`
+                viewContent += `To ${moment(showOnObj["end_time"]).tz("Asia/Hong_Kong").format()}\n`
+              } else if (Object.keys(showOnObj).includes("user_ids")) {
+                viewContent += `User ID: ${(showOnObj["user_ids"] as string[]).join(", ")}\n`
+              } else {
+                console.log("cat" + cat)
+                console.log("showOnObj")
+                console.log(showOnObj)
+              }
+            }
+            viewContent += `\n`
+          }
+
+          viewContent += `| Filename | Emoji | GIF | PNG |\n`
+          viewContent += `| --- | --- | --- | --- |\n`
+
+          for (let element of merge) {
+            let split = element[1].split("/")
+            let name = split[split.length - 1].split(".")[0]
+            let code = element[0], gif = element[1], png = element[2]
+            viewContent += `| ${name} | \`${code}\` | ![${name}](../${gif}) | ![${name}](../${png}) |\n`
+          }
+
+          viewContent += "\n"
+
+          fs.writeFileSync(`./view/${cat}.md`, viewContent)
+
+          mainContent += `| ${cat} | ${mapping[cat] ? mapping[cat] : cat} | ![${merge[0][1]}](${merge[0][1]}) | [View](./view/${cat}.md) |\n`
+
+          allContent += viewContent
         }
 
-        viewContent += `| Filename | Emoji | GIF | PNG |\n`
-        viewContent += `| --- | --- | --- | --- |\n`
+        let readmeTemplate = fs.readFileSync("./README_TEMPLATE", "utf8")
 
-        for (let element of merge) {
-          let split = element[1].split("/")
-          let name = split[split.length - 1].split(".")[0]
-          let code = element[0], gif = element[1], png = element[2]
-          viewContent += `| ${name} | \`${code}\` | ![${name}](../${gif}) | ![${name}](../${png}) |\n`
-        }
+        let result = readmeTemplate.replace("{body}", mainContent);
 
-        viewContent += "\n"
+        fs.writeFileSync("./README.md", result, "utf8")
+        fs.writeFileSync(`./view/all.md`, allContent)
 
-        fs.writeFileSync(`./view/${cat}.md`, viewContent)
-
-        mainContent += `| ${cat} | ${mapping[cat] ? mapping[cat] : cat} | ![${merge[0][1]}](${merge[0][1]}) | [View](./view/${cat}.md) |\n`
-
-        allContent += viewContent
+        // fs.rmdirSync(folderName, {recursive: true})
+        fs.rm(fileName)
       }
-
-      let readmeTemplate = fs.readFileSync("./README_TEMPLATE", "utf8")
-
-      let result = readmeTemplate.replace("{body}", mainContent);
-
-      fs.writeFileSync("./README.md", result, "utf8")
-      fs.writeFileSync(`./view/all.md`, allContent)
-
-      // fs.rmdirSync(folderName, {recursive: true})
-      fs.rm(fileName)
-    }
-  })
+    })
+  }
 })()
 
